13052b6f669fdce9279631527c8a761c92069b1c
KrzysztofNa <krzysztofnachyla@gmail.com>
2021-10-01 22:33:21 +0200

    add two browsers and web scrapper

--- test1.py
+++ test1.py
@@ -1,10 +1,10 @@
-"web browser"
+# web browser with socket
 
 import socket
-
+'''
 mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 mysock.connect(('data.pr4e.org', 80))
-cmd = 'GET http://data.pr4e.org/romeo.txt HTTP/1.0\n\n'.encode()
+cmd = 'GET http://data.pr4e.org/romeo.txt HTTP/1.0\r\n\r\n'.encode()
 mysock.send(cmd)
 
 while True:
@@ -13,4 +13,34 @@ while True:
         break
     print(data.decode())
 mysock.close()
+'''
+# web browser with urllib
+'''
+import urllib.request
+
+fhand = urllib.request.urlopen('http://data.pr4e.org/romeo.txt')
+
+counts = dict()
+for line in fhand:
+    words = line.decode().split()
+    for word in words:
+        counts[word] = counts.get(word, 0) + 1
+print(counts)
+'''
+
+# web scrapping
+
+import urllib.request, urllib.parse, urllib.error
+from bs4 import BeautifulSoup
+
+url = input('Enter - ')
+html = urllib.request.urlopen(url).read()
+soup = BeautifulSoup(html, 'html.parser')
+
+tags = soup('a')
+for tag in tags:
+    print(tag.get('href', None))
+
+
+
 
